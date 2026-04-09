dd2795ce0590ac28a9317d41177637b7d219e3cc
libreria-dafne <libreriadafne2@gmail.com>
2026-04-09 08:23:44 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "dafne-cache-v1";
+
+const urlsToCache = [
+  "index.html",
+  "manifest.json"
+];
+
+// Instalar y guardar archivos
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activar y limpiar caché viejo
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar solicitudes
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
